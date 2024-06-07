194be17a260981515358f88b403ecc59fbd0292a
RootViper4 <fracnkysourya.com>
2024-06-07 12:54:37 +0300

    blabalabla

---
+++ src/app/routes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const postLogin = require("./controllers/users/login");
+const postUser = require("./controllers/users/register");
+const putUserDetails = require("./controllers/users/putUserDetails");
+const authentication = require("./middlewares/authentication");
+const authorise = require("./middlewares/authorisation");
+const getUserTypes = require("./controllers/users/userTypes");
+const { ADMIN } = require("~root/constants/userTypes");
+const putPassword = require("./controllers/password-recovery/putPassword");
+const postRecoveryRequest = require("./controllers/password-recovery/postRecoveryRequest");
+
+const router = express.Router();
+
+// USER MANAGEMENT
+router.post("/login", postLogin);
+
+router.post(
+  "/register",
+  authentication,
+  authorise({ roles: [ADMIN] }),
+  postUser
+);
+
+router.put("/edit/user", authentication, putUserDetails);
+
+router.get("/user-types", getUserTypes);
+
+router.post("/recovery-request", postRecoveryRequest);
+
+router.put("/update-password/:shortcode", putPassword);
+
+module.exports = router;
